03805fbc7bf7c1845184d06f81b476aeb1be2024
OlssonMartin <martin1862@hotmail.com>
2024-09-19 17:42:54 +0200

    Enkel profil utan något

---
+++ src/stories/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div style={{ backgroundColor: "#f0f0f0", height: "100vh" }}>
+      <h1>Profile</h1>
+      <p>Här var det tomt</p>
+    </div>
+  );
+};
+
+export default Profile;
