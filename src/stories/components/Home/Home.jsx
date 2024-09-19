74ae16b94db0501ae53341a1d3d4a85efb76aecd
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2024-09-19 22:38:38 +0200

    Created Home

---
+++ src/stories/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='container'>
+            <h1>Välkommen till TaskMasterAI!</h1>
+            <p>
+            Börja ditt äventyr mot ett mer ordnat liv med TaskMasterAI
+             – så slipper du känna dig som en virrig ekorre som tappat bort sina nötter!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
