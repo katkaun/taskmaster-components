23a45a9983b678748571659d2cca990bda4fab1f
Your Name <you@example.com>
2024-09-19 18:56:57 +0200

    Uppdaterade styling på Login-sidan

--- src/stories/components/Login/Login.jsx
+++ src/stories/components/Login/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./Login.module.css";
+import User from "../Register/User";
+import Password from "../Register/Password";
 
 const Login = () => {
-  return <div className={styles.loginContainer}>Login</div>;
+  return <div className={styles.loginContainer}>
+          <h2 className={styles.title}>Login</h2>
+          
+          <div className={styles.forminput} >
+           <User />
+           <Password/>
+           </div>
+           <button>Login</button>
+  </div>;
 };
 
 export default Login;
