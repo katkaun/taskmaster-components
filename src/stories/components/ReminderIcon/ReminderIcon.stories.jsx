2c16320dfc30c4887f075d8a98b31a27c46fccf0
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-09-23 09:07:41 +0200

    45 - remindericon

---
+++ src/stories/components/ReminderIcon/ReminderIcon.stories.jsx
@@ -0,0 +1,10 @@
+import ReminderIcon from "./ReminderIcon";
+
+export default {
+    title: "taskmaster/components/ReminderIcon",
+    component: ReminderIcon,
+};
+
+export const Default = {
+    
+};
