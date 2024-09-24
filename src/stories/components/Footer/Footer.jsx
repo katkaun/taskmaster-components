383abb428e6e251c5917a814380865ad7a928736
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2024-09-24 21:47:06 +0200

    Upgrade footer

--- src/stories/components/Footer/Footer.jsx
+++ src/stories/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import styles from "./Footer.module.css";
+import React from 'react';
+import './Footer.module.css';
 
-const Footer = () => {
-  return <div className={styles.footerContainer}>footer</div>;
-};
+function Footer() {
+    return (
+            <footer>
+                <h2>TaskMasterAI</h2>
+                <section className="footer-content">
+                    <div className="left-side">
+                        <a href="#">Home</a>
+                        <a href="#">About us</a>
+                        <a href="#">Contact us</a>
+                    </div>
+                    <div className="right-side">
+                        <a href="#">Login</a>
+                        <a href="#">Register</a>
+                        <a href="#">Calendar</a>
+                    </div>
+                        </section>
+                </footer>
+                        );
+                    };
 
-export default Footer;
+    export default Footer;
