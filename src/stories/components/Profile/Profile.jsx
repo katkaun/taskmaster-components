2f168e060bf906f0555819b46bf8429f92aeda43
OlssonMartin <martin1862@hotmail.com>
2024-09-24 15:53:27 +0200

    la till knapp som inte gör något

--- src/stories/components/Profile/Profile.jsx
+++ src/stories/components/Profile/Profile.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import styles from "./Profile.module.css";
 import UserIcon from "../UserIcon/UserIcon"; 
+import Button from "../Button/Button"; 
 
 const Profile = () => {
   return (
     <div className={styles.profileContainer}>
       <div className={styles.profileBody}>
         <div className={styles.profilePicture}>
-          {}
           <UserIcon className={styles.userIcon} />
         </div>
         <h2 className={styles.profileName}>Ditt namn</h2>
         <p className={styles.profileText}></p>
-        
-        {}
+
         <div className={styles.contactInfo}>
-          <p><strong>Användarnamn:</strong> </p>
-          <p><strong>Epost:</strong> @example.se</p>
+          <p><strong>Användarnamn:</strong> johndoe</p>
+          <p><strong>Epost:</strong> john@example.com</p>
           <p><strong>Telefonnummer:</strong> 123-456-789</p>
+        </div>
+
+        <div className={styles.buttonWrapper}>
+          <Button title="Redigera" className={styles.editButton} />
         </div>
       </div>
     </div>
   );
 };
 
-export default Profile; 
+export default Profile;
