02bda48214c0cd6fec1e16f800df176d90b98e7e
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2024-09-26 23:29:47 +0200

    fixade footer, men har strull med logon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import './Footer.module.css';
+import styles from './Footer.module.css';
 
 function Footer() {
-    return (
-            <footer>
-                <h2>TaskMasterAI</h2>
-                <section className="footer-content">
-                    <div className="left-side">
-                        <a href="#">Home</a>
-                        <a href="#">About us</a>
-                        <a href="#">Contact us</a>
-                    </div>
-                    <div className="right-side">
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
-                        <a href="#">Calendar</a>
-                    </div>
-                        </section>
-                </footer>
-                        );
-                    };
+  return (
+    <section className={styles.footer}>
+      <div className={styles.footerRow}>
+        <div className={styles.footerCol}>
+          <ul className={styles.links}>
+            <li>
+              <img src="/armin.jpg" alt="Armin's face" width="250" height="250" />
+            </li>
+          </ul>
+        </div>
+        <div className={styles.footerCol}>
+          <ul className={styles.links}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+          </ul>
+        </div>
+        <div className={styles.footerCol}>
+          <ul className={styles.links}>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Kalender</a></li>
+          </ul>
+        </div>
+        <div className={styles.footerCol}>
+          <h4>TaskMasterAI</h4>
+          <p>
+            Börja ditt äventyr mot ett mer ordnat liv med TaskMaster AI
+          </p>
+          <div className={styles.icons}>
+            <img src="/facebook.png" alt="Facebook" width="50" height="50" />
+            <img src="/youtube.png" alt="YouTube" width="50" height="50" />
+            <img src="/x.png" alt="X" width="50" height="50" />
+            <img src="/instagram.png" alt="Instagram" width="50" height="50" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
-    export default Footer;
+export default Footer;
