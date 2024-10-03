6952af284e6063967c3f140da9bef3a751fa9da9
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2024-10-03 22:32:19 +0200

    CSS stylish done!

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import './Navigation.module.css';
+import styles from './Navigation.module.css';
 
 function Navigation() {
     return (
-        <ul>
+        <ul className={styles.ul}>
             <li><a href="#">About us</a></li>
             <li><a href="#">Updates</a></li>
             <li><a href="#">Contact us</a></li>
-            <li><a href="#">Home</a></li>
+            <li className={styles.floatRight}><a className={styles.active} href="#">Home</a></li>
         </ul>
     );
 }
