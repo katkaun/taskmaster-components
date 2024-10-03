18402015db973275d61ff87dd407a273b24e4f8d
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2024-10-03 17:42:34 +0200

    Created Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Navigation.module.css';
+
+function Navigation() {
+    return (
+        <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Updates</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+    );
+}
+
+export default Navigation;
---
+++ src/components/Navigation/Navigation.stories.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./Navigation";
+
+export default {
+    title: "taskmaster/components/Navigation",
+    component: Navigation,
+}
+
+export const Default = {
+
+}
