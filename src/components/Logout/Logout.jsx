26084487aed46eb9c604c163bcfde92824c9c6da
OlssonMartin <martin1862@hotmail.com>
2024-10-04 08:56:51 +0200

    Fixade merge-konflikter

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React from 'react';
->>>>>>> 6952af284e6063967c3f140da9bef3a751fa9da9
 import styles from './Logout.module.css';
 
 function Logout() {
